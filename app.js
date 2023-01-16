642d5e24e5362f650f15af173079d984a5a0af07
taekhwankim <taekhwankim21>
2023-01-16 19:04:22 +0900

    수정

--- app.js
+++ app.js
@@ -23,85 +23,86 @@
 const express = require("express");
 const app = express();
 
-// 로그인 
+// 로그인
 app.post("/login", (req, res) => {
-  const {email, password} = req.body
-  console.log("email", email)
-  console.log("password", password)
+  const { email, password } = req.body;
+  console.log("email", email);
+  console.log("password", password);
 
-  res.send("/login api")
+  res.send("/login api");
 });
 
-//회원가입 
+//회원가입
 app.post("/signup", (req, res) => {
-  const {email, password, coment} = req.body
-  console.log("email",email)
-  console.log("password",password)
-  console.log("coment",coment)
+  const { email, password, coment } = req.body;
+  console.log("email", email);
+  console.log("password", password);
+  console.log("coment", coment);
 
-  res.send("/signup api")
+  res.send("/signup api");
 });
 
 //회원정보 한명 가져오기
 app.get("/finduser", (req, res) => {
-  const {email, password, coment} = req.params
-  console.log("email",email)
-  console.log("password",password)
-  console.log("coment",coment)
+  const { userId } = req.params;
+  const { email, password, coment } = req.body;
+  console.log("userId", userId);
+  console.log("email", email);
+  console.log("password", password);
+  console.log("coment", coment);
 
-  res.send("/finduser api")
+  res.send("/finduser api");
 });
 
 //게시글 리스트  가져오기
-app.get("/boardlist", (req, res) => {
-  const {nickname, board} =req.body
-  console.log("nickname",nickname)
-  console.log("board",board)
+app.get("/post", (req, res) => {
+  const { nickname, board } = req.body;
+  console.log("nickname", nickname);
+  console.log("board", board);
 
-  res.send("/boardlist api")
+  res.send("/boardlist api");
 });
 
 //선택 게시글 상세 정보 가져오기
-app.get("/board/:id", (req, res) => {
-  const {nickname, title}=req.params
-  console.log("nickname",nickname)
-  console.log("title",title)
+app.get("/post/:id", (req, res) => {
+  const { postId } = req.params;
+  const { nickname, title } = req.body;
+  console.log("postId", postId);
+  console.log("nickname", nickname);
+  console.log("title", title);
 
-  res.send("/board api")
+  res.send("/post/:id");
 });
 
 //게시글 작성하기
-app.post("/post", (req, res) => {
-  const {title, coment, nickname} = req.body
-  console.log("title",title);
-  console.log("coment",coment);
-  console.log("nickname",nickname);
+app.post("/post/create", (req, res) => {
+  const { title, coment, nickname } = req.body;
+  console.log("title", title);
+  console.log("coment", coment);
+  console.log("nickname", nickname);
 
-  res.send("/post api")
-
+  res.send("/post/create api");
 });
 
 //게시글 수정하기
-app.patch("/edit", (req, res) => {
-  const {title, coment, nickname} = req.body
-  console.log("title",title);
-  console.log("coment",coment);
-  console.log("nickname",nickname);
+app.patch("/post/edit", (req, res) => {
+  const { postId } = req.params;
+  const { title, coment, nickname } = req.body;
+  console.log("postId", postId);
+  console.log("title", title);
+  console.log("coment", coment);
+  console.log("nickname", nickname);
 
-  res.send("/edit api")
-
+  res.send("/post/edit api");
 });
 
 //게시글 삭제하기
-app.post("/delete/:Id", (req, res) => {
-  const {postId} = req.params
-  console.log("postId",postId)
-  console.log(req.);
+app.post("/post/delete/:Id", (req, res) => {
+  const { postId } = req.params;
+  console.log("postId", postId);
 
-  res.send("/delete api")
-
+  res.send("/post/delete api");
 });
-
 
 app.listen(3000, () => {
   console.log("웹 서버가 3000번 포트에서 실행되었습니다.");
